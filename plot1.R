d5acfd3813fd7f35530a370094a1e02613de003c
Bodhi Goswami <90079160+bodhigoswami@users.noreply.github.com>
2021-09-26 22:31:12 -0700

    Commit #1

---
+++ plot1.R
@@ -0,0 +1,13 @@
+rm(list=ls())
+library(readr)
+hdp_data <- read_delim("~/My R Work/R Studio Default/data/Individual household electric power consumption Data Set/household_power_consumption.txt",
+                       delim = ";", escape_double = FALSE, trim_ws = TRUE)
+library(lubridate)
+hdp_data$Date<-dmy(hdp_data$Date)
+my_data<-rbind(subset(hdp_data, Date == c("2007-02-01")),
+                subset(hdp_data, Date == c("2007-02-01")))#Only two days required
+my_data$Global_active_power<-as.numeric(my_data$Global_active_power)
+png(filename = "plot1.png")
+hist(my_data$Global_active_power, col = "red", main = "Global Active Power",
+                xlab = "Global Active Power (kilowatts)")
+dev.off()
